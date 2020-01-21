838f7c2e510c6b9d74c013c8bdd70bc6011d6451
Łukasz <jestspoko@gmail.com>
2020-01-21 12:16:38 +0100

    added framework

---
+++ FriendlyScoreCore.framework/Headers/FriendlyScoreCore.h
@@ -0,0 +1,19 @@
+//
+//  FriendlyScoreCore.h
+//  FriendlyScoreCore
+//
+//  Created by Lukasz Czechowicz on 17/01/2020.
+//  Copyright © 2020 Lukasz Czechowicz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FriendlyScoreCore.
+FOUNDATION_EXPORT double FriendlyScoreCoreVersionNumber;
+
+//! Project version string for FriendlyScoreCore.
+FOUNDATION_EXPORT const unsigned char FriendlyScoreCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FriendlyScoreCore/PublicHeader.h>
+
+
