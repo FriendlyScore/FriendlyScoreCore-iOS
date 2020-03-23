041a37fb6e710ce933a13f2230b4eb3b277d6182
Lukasz Czechowicz <jestspoko@gmail.com>
2020-03-23 15:05:04 +0100

    framework update

---
+++ FriendlyScoreConnect.framework/Headers/FriendlyScoreConnect.h
@@ -0,0 +1,19 @@
+//
+//  FriendlyScoreConnect.h
+//  FriendlyScoreConnect
+//
+//  Created by Lukasz Czechowicz on 17/01/2020.
+//  Copyright © 2020 Lukasz Czechowicz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FriendlyScoreConnect.
+FOUNDATION_EXPORT double FriendlyScoreConnectVersionNumber;
+
+//! Project version string for FriendlyScoreConnect.
+FOUNDATION_EXPORT const unsigned char FriendlyScoreConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FriendlyScoreConnect/PublicHeader.h>
+
+
